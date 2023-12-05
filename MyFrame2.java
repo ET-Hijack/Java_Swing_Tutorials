fb3d686dc0f5e5d9e90fce3674f47f31341987c6
ET Comp <115888508+ET-Hijack@users.noreply.github.com>
2023-12-05 20:27:33 +0000

    Creating a Frame by Composition

---
+++ MyFrame2.java
@@ -0,0 +1,9 @@
+import java.awt.Frame;
+
+public class MyFrame2 {
+    public static void main(String args[]){
+        Frame frame = new Frame("My Frame Title");
+        frame.setSize(500,400);
+        frame.setVisible(true);
+    }
+}
